cdd80d2b11839ddb010b509c7718cff881c829e8
S N Shekhar Das <dasnsu@gmail.com>
2020-03-17 16:50:49 +0600

    Enable to share button

--- app/src/main/java/com/das/myapplication/SecondActivity.kt
+++ app/src/main/java/com/das/myapplication/SecondActivity.kt
@@ -2,11 +2,18 @@ package com.das.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle : Bundle? = intent.extras
+        val msg = bundle?.getString("user_message")
+        show_message.text = msg
     }
 }
